9348156f8e2c05ef813e09199842e4893ab43fa8
Albert <albert_zju@163.com>
2020-10-19 22:25:37 +0800

    feat: 添加基础第二小题示例代码

--- eight-basic/src/App.js
+++ eight-basic/src/App.js
@@ -1,3 +1,4 @@
+/* 第一小题示例代码 */
 import React, { Component } from 'react';
 import './App.css';
 
@@ -9,3 +10,17 @@ class App extends Component {
 }
 
 export default App;
+
+
+/* 第二小题示例代码 */
+// import { Button } from 'antd';
+// import schema from './schema';
+// import RenderEngine from './render-engine';
+// import './App.css';
+
+// const components = { Button };
+// // 渲染引擎的输入为页面描述 schema 和组件依赖的映射 components
+// const re = new RenderEngine(schema, components);
+// const App = re.createApp();
+
+// export default App;
